c8fbe7375efff56296cba2fd20fccc315360278a
quangdethuong <quangnn1106@gmail.com>
2023-09-18 22:28:09 +0700

    init

---
+++ BusinessObjects/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class Products
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public int UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
